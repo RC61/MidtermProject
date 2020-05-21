bb63750fa124ec2956adca31d56353bcf9846190
RC61 <jetwestpack@gmail.com>
2020-05-21 11:28:59 -0600

    boot project created

---
+++ CaninesAndKoozies/src/main/java/com/skilldistillery/caninesandkoozies/CaninesAndKooziesApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.caninesandkoozies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaninesAndKooziesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaninesAndKooziesApplication.class, args);
+	}
+
+}
---
+++ CaninesAndKoozies/src/test/java/com/skilldistillery/caninesandkoozies/CaninesAndKooziesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.caninesandkoozies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaninesAndKooziesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
