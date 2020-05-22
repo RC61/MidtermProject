52d4fd8f8ee47df8b0fe5b89afa73fa686742065
RC61 <jetwestpack@gmail.com>
2020-05-22 16:20:47 -0600

    create dog test

---
+++ JPACanineKoozie/src/test/java/com/skilldistillery/caninesandkoozies/entities/DogTest.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.caninesandkoozies.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DogTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Dog dog;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("CanineKoozie");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		dog = em.find(Dog.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		dog = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(dog);
+		assertEquals("Baxter", dog.getName());
+		assertEquals("Shihtzu", dog.getBreed());
+		assertEquals("small", dog.getSize());
+		assertEquals(null, dog.getDogPicture());
+	}
+
+}
