6a300f6bf87f97a51fd0f89e6eb21735070edd98
Tabatha Flores <tabathaflores@SDMacBookPro2.local>
2020-05-22 14:05:31 -0600

    updated DB

--- DB/caninesandkooziesdb.sql
+++ DB/caninesandkooziesdb.sql
@@ -113,9 +113,8 @@ DROP TABLE IF EXISTS `event` ;
 CREATE TABLE IF NOT EXISTS `event` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(1000) NOT NULL,
-  `event_date` DATE NOT NULL,
-  `event_time` TIME NOT NULL,
-  `dog_size_preference` VARCHAR(1000) NOT NULL,
+  `event_date_time` DATETIME NOT NULL,
+  `dog_size_preference` VARCHAR(1000) NOT NULL DEFAULT 'none',
   `single_only_preference` TINYINT NOT NULL DEFAULT 1,
   `picture_url` VARCHAR(8000) NULL,
   `description` VARCHAR(10000) NOT NULL,
@@ -207,6 +206,12 @@ START TRANSACTION;
 USE `caninesandkooziesdb`;
 INSERT INTO `address` (`id`, `street`, `city`, `state`, `zip_code`) VALUES (1, '123 Main Street', 'Englewood', 'CO', 80110);
 INSERT INTO `address` (`id`, `street`, `city`, `state`, `zip_code`) VALUES (2, '678 Venue Lane', 'Denver', 'CO', 80104);
+INSERT INTO `address` (`id`, `street`, `city`, `state`, `zip_code`) VALUES (3, '78 North Littleton Avenue', 'Littleton', 'CO', 80113);
+INSERT INTO `address` (`id`, `street`, `city`, `state`, `zip_code`) VALUES (4, '98 Leper Way', 'Denver', 'CO', 80112);
+INSERT INTO `address` (`id`, `street`, `city`, `state`, `zip_code`) VALUES (5, '767 Skill Distillery Road', 'Greenwood Village', 'CO', 80112);
+INSERT INTO `address` (`id`, `street`, `city`, `state`, `zip_code`) VALUES (6, '1695 Platte Street', 'Denver', 'CO', 80202);
+INSERT INTO `address` (`id`, `street`, `city`, `state`, `zip_code`) VALUES (7, '3563 Wazee Street\n', 'Denver', 'CO', 80216);
+INSERT INTO `address` (`id`, `street`, `city`, `state`, `zip_code`) VALUES (8, '201 West Colfax Avenue', 'Denver', 'CO', 80202);
 
 COMMIT;
 
@@ -218,6 +223,9 @@ START TRANSACTION;
 USE `caninesandkooziesdb`;
 INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `first_name`, `last_name`, `birth_date`, `relationship_status`, `email`, `alcohol_preference`, `picture_url`, `create_date`, `update_date`, `address_id`) VALUES (1, 'admin', 'admin', 1, NULL, 'admin', 'admin', '2020-01-01', NULL, 'admin@admin.com', NULL, NULL, '2020-01-01 10:10:10', '2020-01-01 10:10:10', 1);
 INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `first_name`, `last_name`, `birth_date`, `relationship_status`, `email`, `alcohol_preference`, `picture_url`, `create_date`, `update_date`, `address_id`) VALUES (2, 'tabathaf', 'tabathaf', 1, NULL, 'Tabatha', 'Flores', '1991-03-18', 'married', 'tabatha.flores18@gmail.com', 'whiskey', NULL, '2020-05-22 11:23:00', NULL, 2);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `first_name`, `last_name`, `birth_date`, `relationship_status`, `email`, `alcohol_preference`, `picture_url`, `create_date`, `update_date`, `address_id`) VALUES (3, 'aflo13', 'agathaf', 1, NULL, 'Agatha', 'Flores', '1988-12-13', '', 'aaf1213@gmail.com', 'wine', NULL, '2020-05-22 11:56:00', NULL, 3);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `first_name`, `last_name`, `birth_date`, `relationship_status`, `email`, `alcohol_preference`, `picture_url`, `create_date`, `update_date`, `address_id`) VALUES (4, 'chadmurray', 'chadmurray', 1, NULL, 'Chad', 'Murray', '1990-01-23', 'single', 'chadmurry@gmail.com', 'beer', NULL, '2020-05-22 13:14:00', NULL, 4);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `first_name`, `last_name`, `birth_date`, `relationship_status`, `email`, `alcohol_preference`, `picture_url`, `create_date`, `update_date`, `address_id`) VALUES (5, 'tpapp', 'tpapp', 1, NULL, 'Toni', 'Papp', '1991-07-14', 'single', 'tpapp@gmail.com', 'liquor', NULL, '2020-05-22 13:18:00', NULL, 5);
 
 COMMIT;
 
@@ -227,7 +235,11 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `caninesandkooziesdb`;
-INSERT INTO `dog` (`id`, `name`, `breed`, `size`, `picture_url`, `user_id`) VALUES (1, 'Baxter', 'Shihtzu', 'small', NULL, 2);
+INSERT INTO `dog` (`id`, `name`, `breed`, `size`, `picture_url`, `user_id`) VALUES (1, 'Baxter', 'Shihtzu', 'small', 'https://i.imgur.com/B4eo1gU.jpg', 2);
+INSERT INTO `dog` (`id`, `name`, `breed`, `size`, `picture_url`, `user_id`) VALUES (2, 'Max', 'Bichon Frise', 'medium', 'https://i.imgur.com/fLZSNCU.jpg', 3);
+INSERT INTO `dog` (`id`, `name`, `breed`, `size`, `picture_url`, `user_id`) VALUES (3, 'Jasper', 'Golder Retriever', 'large', NULL, 4);
+INSERT INTO `dog` (`id`, `name`, `breed`, `size`, `picture_url`, `user_id`) VALUES (4, 'Luca', 'Bichon Frise', 'small', NULL, 5);
+INSERT INTO `dog` (`id`, `name`, `breed`, `size`, `picture_url`, `user_id`) VALUES (5, 'Ziki', 'Pekingese', 'small', NULL, 5);
 
 COMMIT;
 
@@ -238,6 +250,9 @@ COMMIT;
 START TRANSACTION;
 USE `caninesandkooziesdb`;
 INSERT INTO `venue` (`id`, `name`, `alcohol_provided`, `picture_url`, `venue_link`, `address_id`) VALUES (1, 'Sample Venue', 1, NULL, NULL, 2);
+INSERT INTO `venue` (`id`, `name`, `alcohol_provided`, `picture_url`, `venue_link`, `address_id`) VALUES (2, 'Denver Beer Co', 1, 'https://i.imgur.com/OhmqRiF.png', 'https://denverbeerco.com/', 6);
+INSERT INTO `venue` (`id`, `name`, `alcohol_provided`, `picture_url`, `venue_link`, `address_id`) VALUES (3, 'Bigsby\'s Folly', 1, 'https://i.imgur.com/Cya5ijB.png', 'https://www.bigsbysfolly.com/', 7);
+INSERT INTO `venue` (`id`, `name`, `alcohol_provided`, `picture_url`, `venue_link`, `address_id`) VALUES (4, 'Wellshire Golf Course', 0, 'https://i.imgur.com/LMxFnN2.jpg', 'https://www.cityofdenvergolf.com/wellshire', 8);
 
 COMMIT;
 
@@ -247,7 +262,8 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `caninesandkooziesdb`;
-INSERT INTO `event` (`id`, `name`, `event_date`, `event_time`, `dog_size_preference`, `single_only_preference`, `picture_url`, `description`, `create_date`, `update_date`, `venue_id`, `creator_id`) VALUES (1, 'Wine and Pup', '2020-06-01', '16:00:00 ', 'small', 1, NULL, 'Drink wine and bring pups', '2020-05-22', NULL, 1, 2);
+INSERT INTO `event` (`id`, `name`, `event_date_time`, `dog_size_preference`, `single_only_preference`, `picture_url`, `description`, `create_date`, `update_date`, `venue_id`, `creator_id`) VALUES (1, 'Wine and Pup', '2020-06-01 16:00:00', 'small', 1, NULL, 'Drink wine and bring pups', '2020-05-22', NULL, 1, 2);
+INSERT INTO `event` (`id`, `name`, `event_date_time`, `dog_size_preference`, `single_only_preference`, `picture_url`, `description`, `create_date`, `update_date`, `venue_id`, `creator_id`) VALUES (2, 'iPupAs', '2020-07-01 17:00:00', 'none', 0, 'https://i.imgur.com/vTI4oDl.jpg', 'Bring your pups (all welcome) and join us in an ipa socialization event! Bring a friend to join in on the fun. There will be small snacks provided in the venue. All riverblend IPAs and well drinks will be 50% off.', '2020-05-22', NULL, 2, 3);
 
 COMMIT;
 
