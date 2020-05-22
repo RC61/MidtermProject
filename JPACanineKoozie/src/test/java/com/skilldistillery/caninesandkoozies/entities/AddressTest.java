18b6d73926e3df5cddf5519487072036ae9ff9f8
Tabatha Flores <tabathaflores@SDMacBookPro2.local>
2020-05-22 16:06:30 -0600

    address test

--- JPACanineKoozie/src/test/java/com/skilldistillery/caninesandkoozies/entities/AddressTest.java
+++ JPACanineKoozie/src/test/java/com/skilldistillery/caninesandkoozies/entities/AddressTest.java
@@ -2,6 +2,10 @@ package com.skilldistillery.caninesandkoozies.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,25 +13,39 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AddressTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Address address;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("CanineKoozie");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		address = em.find(Address.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		address = null;
 	}
 
 	@Test
-	void test() {
+	void test_Address_entity_mapping() {
+		assertNotNull(address);
+		assertEquals("123 Main Street", address.getStreet());
+		assertEquals("Englewood", address.getCity());
+		assertEquals("CO", address.getState());
+		assertEquals(80110, address.getZipCode());
 		
 	}
 
