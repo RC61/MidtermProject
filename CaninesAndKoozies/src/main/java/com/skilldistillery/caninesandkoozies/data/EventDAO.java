4d0ceb3e31891afe9bb67b7d611e220d33db9221
Tabatha Flores <tabathaflores@SDMacBookPro2.local>
2020-05-23 15:56:57 -0600

    Event DAO

--- CaninesAndKoozies/src/main/java/com/skilldistillery/caninesandkoozies/data/EventDAO.java
+++ CaninesAndKoozies/src/main/java/com/skilldistillery/caninesandkoozies/data/EventDAO.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.caninesandkoozies.data;
 
+import java.util.List;
+
 import com.skilldistillery.caninesandkoozies.entities.Event;
 
 public interface EventDAO {
@@ -8,4 +10,11 @@ public interface EventDAO {
 	
 	public Event updateEvent(Event event);
 
+	public boolean destroy(int id);
+	
+	public List<Event> findAllEvents();
+	
+	public List<Event> eventsBySinglesOnlyPreference(Boolean singlesOnly);
+	
+	public List<Event> eventsByDogSizePreference(String dogSizePref);
 }
--- CaninesAndKoozies/src/main/java/com/skilldistillery/caninesandkoozies/data/EventDAOImpl.java
+++ CaninesAndKoozies/src/main/java/com/skilldistillery/caninesandkoozies/data/EventDAOImpl.java
@@ -1,5 +1,94 @@
 package com.skilldistillery.caninesandkoozies.data;
 
-public class EventDAOImpl {
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.caninesandkoozies.entities.Event;
+import com.skilldistillery.caninesandkoozies.entities.User;
+import com.skilldistillery.caninesandkoozies.entities.Venue;
+
+@Service
+@Transactional
+public class EventDAOImpl implements EventDAO {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public Event createEvent(Event event) {
+
+		event.setUserCreated(em.find(User.class, event.getUserCreated()));
+		event.setVenue(em.find(Venue.class, event.getVenue()));
+
+		em.persist(event);
+		em.flush();
+		em.close();
+
+		return event;
+	}
+
+	public List<Event> findAllEvents() {
+		String jpql = "SELECT e FROM Event e";
+		List<Event> events;
+		events = em.createQuery(jpql, Event.class).getResultList();
+
+		em.close();
+		return events;
+	}
+
+	public List<Event> eventsBySinglesOnlyPreference(Boolean singlesOnly) {
+		List<Event> events;
+		String jpql = "SELECT e FROM Event e WHERE e.singleOnlyPreference = :true";
+		events = em.createQuery(jpql, Event.class).setParameter("true", singlesOnly).getResultList();
+
+		return events;
+	}
+
+	public List<Event> eventsByDogSizePreference(String dogSizePref) {
+		List<Event> events;
+		String jpql = "SELECT e FROM Event e WHERE e.dog_size_preference = :dogSize";
+		events = em.createQuery(jpql, Event.class).setParameter("dogSize", dogSizePref).getResultList();
+
+		return events;
+	}
+
+	public Event updateEvent(Event event) {
+
+		Event updatedEvent = em.find(Event.class, event.getId());
+		updatedEvent.setName(event.getName());
+		updatedEvent.setEventDateTime(event.getEventDateTime());
+		updatedEvent.setDogSizePreference(event.getDogSizePreference());
+		updatedEvent.setSingleOnlyPreference(event.isSingleOnlyPreference());
+		updatedEvent.setPictureURL(event.getPictureURL());
+		updatedEvent.setDescription(event.getDescription());
+		updatedEvent.setCreateDate(event.getCreateDate());
+		updatedEvent.setUpdateDate(event.getUpdateDate());
+		updatedEvent.setComments(event.getComments());
+		updatedEvent.setVenue(event.getVenue());
+		updatedEvent.setUserCreated(event.getUserCreated());
+		updatedEvent.setUserEvents(event.getUserEvents());
+
+		em.flush();
+		em.close();
+
+		return updatedEvent;
+	}
+
+	public boolean destroy(int id) {
+		Event event = em.find(Event.class, id);
+
+		em.remove(event);
+
+		boolean stillContains = em.contains(event);
+
+		em.flush();
+		em.close();
+
+		return !stillContains;
+	}
 
 }
