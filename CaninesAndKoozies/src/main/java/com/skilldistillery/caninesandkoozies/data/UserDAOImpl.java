de1291afced7687b8ecc162941b6bfa671af952e
RC61 <jetwestpack@gmail.com>
2020-05-23 14:54:06 -0600

    further into USERDAO and USERDAOIMPL

--- CaninesAndKoozies/src/main/java/com/skilldistillery/caninesandkoozies/data/UserDAOImpl.java
+++ CaninesAndKoozies/src/main/java/com/skilldistillery/caninesandkoozies/data/UserDAOImpl.java
@@ -27,6 +27,28 @@ public class UserDAOImpl implements UserDAO{
 		em.flush();
 		return newUser;
 	}
+	
+	@Override
+	public User updateUser(int id, User user) {
+		
+		User updatedUser = em.find(User.class, id);
+		
+		updatedUser.setFname(user.getFname());
+		updatedUser.setLname(user.getLname());
+		updatedUser.setAddress(user.getAddress());
+		updatedUser.setAlcoholPreference(user.getAlcoholPreference());
+		updatedUser.setEmail(user.getEmail());
+		return updatedUser;
+	}
+	
+	@Override
+	public boolean deleteUser(int id) {
+		em.remove(em.find(User.class, id));
+		em.flush();
+		boolean stillContains = em.contains(em.find(User.class, id));
+		System.out.println(stillContains);
+		return !stillContains;
+	}
 
 	@Override
 	public Dog createDog(Dog newDog) {
@@ -34,7 +56,34 @@ public class UserDAOImpl implements UserDAO{
 		em.flush();
 		return newDog;
 	}
+	
+	@Override
+	public Dog updateDog(int id, Dog dog) {
+		Dog updatedDog = em.find(Dog.class, id);
+		
+		updatedDog.setName(dog.getName());
+		updatedDog.setSize(dog.getSize());
+		updatedDog.setDogPicture(dog.getDogPicture());
+		return updatedDog;
+	}
+	
+	@Override
+	public boolean deleteDog(int id) {
+		em.remove(em.find(Dog.class, id));
+		em.flush();
+		boolean stillContains = em.contains(em.find(Dog.class, id));
+		System.out.println(stillContains);
+		return false;
+	}
+	
+	// COME BACK TO FINISH ME
+	@Override
+	public List<Dog> findAllUserDogs() {
+//		String jpql = "SELECT "
+		return null;
+	}
 
+	
 	@Override
 	public Event createEvent(Event newEvent) {
 		em.persist(newEvent);
@@ -42,51 +91,23 @@ public class UserDAOImpl implements UserDAO{
 		return newEvent;
 	}
 
-	@Override
-	public User updateUser(int id, User user) {
-		
-		User updatedUser = em.find(User.class, id);
-		
-		updatedUser.setFname(user.getFname());
-		return null;
-	}
-
-	@Override
-	public boolean deleteUser(int id) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Dog updateDog(int id, Dog dog) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean deleteDog(int id) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public List<Dog> findAllUserDogs() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	// FINISH ME
 	@Override
 	public Event updateEvent(int id, Event event) {
-		// TODO Auto-generated method stub
+		Event updatedEvent = em.find(Event.class, id);
 		return null;
 	}
 
 	@Override
 	public boolean deleteEvent(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		em.remove(em.find(Event.class, id));
+		em.flush();
+		boolean stillContains = em.contains(em.find(Event.class, id));
+		System.out.println(stillContains);
+		return !stillContains;
 	}
 
+	//FINISH ME
 	@Override
 	public List<Event> findAllCreatedEvents() {
 		// TODO Auto-generated method stub
