5c68c28acb131c4f3302d049161d8f73d31f87c1
RC61 <jetwestpack@gmail.com>
2020-05-23 14:13:20 -0600

    some guts added to USERDAO and USERDAOIMPL

--- CaninesAndKoozies/src/main/java/com/skilldistillery/caninesandkoozies/data/UserDAO.java
+++ CaninesAndKoozies/src/main/java/com/skilldistillery/caninesandkoozies/data/UserDAO.java
@@ -1,5 +1,24 @@
 package com.skilldistillery.caninesandkoozies.data;
 
-public interface UserDAO {
+import java.util.List;
 
+import com.skilldistillery.caninesandkoozies.entities.Dog;
+import com.skilldistillery.caninesandkoozies.entities.User;
+
+import antlr.debug.Event;
+
+public interface UserDAO {
+	public User createUser(User newUser);
+	public User updateUser(int id, User user);
+	public boolean deleteUser(int id);
+	public Dog createDog(Dog newDog);
+	public Dog updateDog(int id, Dog dog);
+	public boolean deleteDog(int id);
+	public List<Dog> findAllUserDogs();
+	public Event createEvent(Event newEvent);
+	public Event updateEvent(int id, Event event);
+	public boolean deleteEvent(int id);
+	public List<Event> findAllCreatedEvents();
+	
+	// HOW DO I WRITE THIS SHIT FOR EVENTS THE USER WILL ATTEND
 }
--- CaninesAndKoozies/src/main/java/com/skilldistillery/caninesandkoozies/data/UserDAOImpl.java
+++ CaninesAndKoozies/src/main/java/com/skilldistillery/caninesandkoozies/data/UserDAOImpl.java
@@ -1,5 +1,96 @@
 package com.skilldistillery.caninesandkoozies.data;
 
-public class UserDAOImpl {
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.caninesandkoozies.entities.Dog;
+import com.skilldistillery.caninesandkoozies.entities.User;
+
+import antlr.debug.Event;
+
+
+@Service
+@Transactional
+public class UserDAOImpl implements UserDAO{
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public User createUser(User newUser) {
+		em.persist(newUser);
+		em.flush();
+		return newUser;
+	}
+
+	@Override
+	public Dog createDog(Dog newDog) {
+		em.persist(newDog);
+		em.flush();
+		return newDog;
+	}
+
+	@Override
+	public Event createEvent(Event newEvent) {
+		em.persist(newEvent);
+		em.flush();
+		return newEvent;
+	}
+
+	@Override
+	public User updateUser(int id, User user) {
+		
+		User updatedUser = em.find(User.class, id);
+		
+		updatedUser.setFname(user.getFname());
+		return null;
+	}
+
+	@Override
+	public boolean deleteUser(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Dog updateDog(int id, Dog dog) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteDog(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Dog> findAllUserDogs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Event updateEvent(int id, Event event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteEvent(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Event> findAllCreatedEvents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
