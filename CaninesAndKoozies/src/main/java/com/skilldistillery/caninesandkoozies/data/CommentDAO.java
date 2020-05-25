6396b818ddc638f54134f473a3b201d054f4c161
arpapp <trpapp@gmail.com>
2020-05-25 09:33:18 -0600

    dat comment dao she done

--- CaninesAndKoozies/src/main/java/com/skilldistillery/caninesandkoozies/data/CommentDAO.java
+++ CaninesAndKoozies/src/main/java/com/skilldistillery/caninesandkoozies/data/CommentDAO.java
@@ -1,5 +1,13 @@
 package com.skilldistillery.caninesandkoozies.data;
 
+import java.util.List;
+
+import com.skilldistillery.caninesandkoozies.entities.Comment;
+
 public interface CommentDAO {
 
+	public Comment findCommentById(int id);
+	public List<Comment> findAllEventComments(int id);
+	public Comment updateComment(Comment comment);
+	public boolean destroyComment(int id) ;
 }
